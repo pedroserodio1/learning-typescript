d31f5ff1bd31ec6935959a51f9a5dcd68746c8eb
Pedro Serodio <serodiomg@gmail.com>
2021-12-09 21:49:40 -0300

    Tipos

---
+++ types.ts
@@ -0,0 +1,40 @@
+//bollean(true / false)
+let isOpen: boolean
+isOpen = true
+
+// string ('foo', "foo", `foo`)
+let message: string
+message = 'foo'
+
+//number (todos numeors(float, int, hexdecimal, binario))
+let total: number
+total = 10
+
+//array (type[])
+let itens: number[]
+itens = [1, 2, 3]
+
+let values: Array<number>
+values=[1, 2, 3]
+
+//tuple 
+let title: [number, string]
+title = [1, 'foo']
+
+//enum (chave - valor)
+enum Colors{
+    white = '#fff',
+    black = '#000'
+}
+
+//any (qualquer coisa) NÃO E LEGAL!
+let coisa: any
+coisa = 'foo'
+
+// void (vazio) não retorna nada
+function logger(): void{
+    console.log('foo')
+}
+
+//null / undefined
+type Bla = string | undefined
